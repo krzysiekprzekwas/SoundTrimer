4600b51332f63c905ce4ff5128f13dc4520579cc
Krynio <przekwaskrzysiek@gmail.com>
2018-01-29 18:49:46 +0100

    Implemented basic player functionality

--- SoundTrimer/MainWindow.xaml.cs
+++ SoundTrimer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SoundTrimer
 {
@@ -20,9 +22,64 @@ namespace SoundTrimer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+
+        private DispatcherTimer timer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
+            mediaPlayer.MediaOpened += MediaPlayer_MediaOpened;
+        }
+
+        private void MediaPlayer_MediaOpened(object sender, EventArgs e)
+        {
+            lblStatus.Content = String.Format("{0} / {1}", mediaPlayer.Position.ToString(@"mm\:ss"), mediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+
+            sliProgress.Minimum = 0;
+            sliProgress.Maximum = mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (mediaPlayer.Source != null)
+            {
+                lblStatus.Content = String.Format("{0} / {1}", mediaPlayer.Position.ToString(@"mm\:ss"), mediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+                sliProgress.Value = mediaPlayer.Position.TotalSeconds;
+            }
+            else
+                lblStatus.Content = "00:00/00:00";
+        }
+        
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaPlayer.Open(new Uri(openFileDialog.FileName));
+                lblTitle.Content = System.IO.Path.GetFileName(openFileDialog.FileName);
+            }
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += timer_Tick;
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Play();
+            timer.Start();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Pause();
+            timer.Stop();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
         }
     }
 }
